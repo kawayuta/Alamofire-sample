4e3d0333b24c5274e388536c847b6a911d066009
mnshey <k216q013@stu.kyoto-seika.ac.jp>
2017-05-16 16:23:31 +0900

    Initial Commit

---
+++ FRUS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FRUS
+//
+//  Created by かわゆた on 2017/05/16.
+//  Copyright © 2017 かわゆた. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
