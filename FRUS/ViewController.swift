e7fffd422a815ce16ce19623b048c70b91bd0aec
mnshey <k216q013@stu.kyoto-seika.ac.jp>
2017-05-16 18:17:25 +0900

    initial

--- FRUS/ViewController.swift
+++ FRUS/ViewController.swift
@@ -7,12 +7,37 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        Alamofire.request("http://192.168.122.143:3000/babies/1.json").responseJSON { response in
+            print(response.request ?? "ee")  // original URL request
+            print(response.response ?? "ee") // HTTP URL response
+            print(response.data ?? "ee")     // server data
+            print(response.result)   // result of response serialization
+            let jsonDict = response.result.value as! NSDictionary
+            
+            print(jsonDict)
+            
+            let team_1 = jsonDict["team_1"] as! Int
+            let team_2 = jsonDict["team_2"] as! Int
+            let team_3 = jsonDict["team_3"] as! Int
+            let team_4 = jsonDict["team_4"] as! Int
+            
+            
+            print(team_1)
+            print(team_2)
+            print(team_3)
+            print(team_4)
+            
+        }
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
---
+++ Pods/Target Support Files/Pods-FRUS/Pods-FRUS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FRUS : NSObject
+@end
+@implementation PodsDummy_Pods_FRUS
+@end
